72bd5f4e9a1aa95164b3fccd7e7f63728906de49
start.vaadin.com <no-reply@vaadin.com>
2024-05-02 14:16:39 +0000

    Generated project

---
+++ src/main/java/com/dummy/application/views/ec2info/Ec2InfoView.java
@@ -0,0 +1,36 @@
+package com.dummy.application.views.ec2info;
+
+import com.dummy.application.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+
+@PageTitle("Ec2Info")
+@Route(value = "", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class Ec2InfoView extends VerticalLayout {
+
+    public Ec2InfoView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
